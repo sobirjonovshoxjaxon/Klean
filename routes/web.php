c3f3790f18f3710db290a8a4426063defb8021f1
shoxjaxon <shohjahonsobirjonov76@gmail.com>
2026-01-24 22:47:12 +0500

    Men Routelar bilan ishladim

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,45 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/user', [UserController::class, 'index'])->name('user.index');
+Route::get('/test', function(){
+
+    return view('test');
+});
+
+Route::post('contact', function(){
+
+    return "post route";
+
+});
+
+
+
+
+Route::get('/birinchi', function(){
+
+    return "Birinchi";
+});
+
+Route::get('/ikkinchi', function(){
+
+    return "Ikkinchi";
+});
+
+Route::redirect('/birinchi', '/ikkinchi');
+
+
+Route::get('/users/{id?}', function($id = null){
+
+    return "Bu bizning $id userimiz";
+})->name('userlar');
+
+
+
+

7cdea68cdd2d966aca9494282cff8a7b646897d0
shoxjaxon <shoxjaxonsobirjonov77@gmail.com>
2026-03-19 16:00:49 +0500

    I have made Post model and I did Sofdelete inside of Post Model and migration to Furthermore I made model Comment -msfc

--- database/migrations/2026_03_04_115658_create_posts_table.php
+++ database/migrations/2026_03_04_115658_create_posts_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('short_content');
             $table->text('content');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
--- database/migrations/2026_03_19_104643_create_comments_table.php
+++ database/migrations/2026_03_19_104643_create_comments_table.php
@@ -11,8 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('posts', function (Blueprint $table) {
-            $table->dropColumn('image');
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
         });
     }
 
@@ -21,8 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('posts', function (Blueprint $table) {
-            $table->string('image')->after('title');
-        });
+        Schema::dropIfExists('comments');
     }
 };
